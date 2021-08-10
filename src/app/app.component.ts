b3b27cc8715f1fc0e25aa0d7ba3cd701932001f5
Aileen Bag-ao Tandoc <tandocaileen@gmail.com>
2021-08-10 20:41:25 +0800

    aai
    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { onAuthUIStateChange, CognitoUserInterface, AuthState} from 
+'@aws-amplify/ui-components';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'eng';
+  user: CognitoUserInterface | undefined;
+  authState!: AuthState;
+
+  constructor(private ref: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData as CognitoUserInterface;
+      this.ref.detectChanges();
+    })
+  }
+
+  ngOnDestroy() {
+    return onAuthUIStateChange;
+  }
 }
+
+
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import 'zone.js';
+import Amplify, {Auth} from 'aws-amplify';
+import awsconfig from './aws-exports';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  Amplify.configure(awsconfig);
+  Auth.configure(awsconfig);
